44fe95453e90330343b8a6612df59e4f60930a56
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2021-10-09 04:56:42 +0330

    client web app

---
+++ web/get-min-price-2.js
@@ -0,0 +1,5 @@
+﻿var maxp = parseFloat(document.querySelectorAll('.text.fullwidth')[1].innerText);
+var minp = parseFloat(document.querySelectorAll('.text.fullwidth')[2].innerText);
+var baseprc = parseFloat(document.querySelectorAll('.price')[1].innerText);
+var res = (maxp + minp + baseprc) / 3;
+alert(res);
---
+++ web/get-min-price.js
@@ -0,0 +1,27 @@
+﻿var prcol = document.querySelectorAll('.bid-data-inner .number');
+//document.querySelectorAll('.c-exchange-fixedheight')[3].querySelectorAll('.body-price');
+var totalprc = 0;
+var baseprc = parseFloat(document.querySelectorAll('.price')[1].innerText);
+var dvcounter = 0;
+for (var i = 0; i <= prcol.length - 1; i++) {
+    if (baseprc > parseFloat(prcol[i].innerText)) {
+        totalprc += parseFloat(prcol[i].innerText);
+        dvcounter++;
+    }
+}
+var res1 = parseFloat(totalprc / dvcounter);
+
+var prcol = document.querySelectorAll('.c-exchange-fixedheight')[3].querySelectorAll('.body-price');
+var totalprc = 0;
+var baseprc = parseFloat(document.querySelectorAll('.price')[1].innerText);
+var dvcounter = 0;
+for (var i = 0; i <= prcol.length - 1; i++) {
+    //if (baseprc > parseFloat(prcol[i].innerText)) {
+        totalprc += parseFloat(prcol[i].innerText);
+        dvcounter++;
+    //}
+}
+var res2 = parseFloat(totalprc / dvcounter);
+
+var res = (res1 + res2) / 2;
+console.log(res);
